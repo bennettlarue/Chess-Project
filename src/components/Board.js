69f9ae549888a522070ceee9137146e43e5e11bd
begen81 <122930229+bennettlarue@users.noreply.github.com>
2023-03-28 16:36:45 -0400

    Added Comments

--- src/components/Board.js
+++ src/components/Board.js
@@ -12,7 +12,7 @@ const Board = () => {
   const [selectedSpace, setSelectedSpace] = useState(null); // Space selected by the user for the selected piece to move to.
   const [pieceToMove, setPieceToMove] = useState(null); // Piece selected by the user.
   const [animating, setAnimating] = useState(false); // True while a piece is playing a movement animation.
-  const [colorToMove, setColorToMove] = useState("B"); // "W" when it's white's turn, "B" when it's black's turn.
+  const [colorToMove, setColorToMove] = useState("W"); // "W" when it's white's turn, "B" when it's black's turn.
   const [availableSpaces, setAvailableSpaces] = useState([]);
 
   const [board, setBoard] = useState([
@@ -26,46 +26,48 @@ const Board = () => {
     "BR", "BN", "BB", "BK", "BQ", "BB", "BN", "BR",
   ]);
   
-  /*
-  
-  */
+ 
   function handleClick(space) {
     // Check if the space contains a piece and if the piece belongs to current color. 
     if (board[space] != null && board[space][0] == colorToMove) {
       setPieceToMove([space]); // Select piece on this space.
-      setAvailableSpaces(movement.getAvailableMoves(space, board))
+      setAvailableSpaces(movement.getAvailableMoves(space, board)) // Get all moves that this piece can make.
       return true;
     }
 
+    // Check if the user has already selected a piece, and if the space they selected is legal.
     else if (pieceToMove != null && availableSpaces.includes(space)) {
-      let letters = 'abcdefgh';
-      setAvailableSpaces([]);
-      movePiece(space);
+      setAvailableSpaces([]); // Clear available moves.
+      movePiece(space); 
       switchColorToMove();
+      return true;
     }
+
+    else return false;
   }
 
   function movePiece(space) {
     let info = document.getElementById("game-info-text");
     let moveDiv = document.createElement("div");
     let text = document.createTextNode(moveToString(space));
-    moveDiv.appendChild(text);
+    moveDiv.appendChild(text);  // Create a new div with the current move as its text.
     info.appendChild(moveDiv);
 
     setSelectedSpace([space]);
-    setAnimating(true);
+    setAnimating(true); // Animate piece move transition.
 
-      setTimeout(() => {
+      setTimeout(() => { // Execute after piece has finished animation.
         setAnimating(false);
-        const newBoard = board.slice();
-        newBoard[space] = board[pieceToMove];
-        newBoard[pieceToMove] = null;
+        const newBoard = board.slice(); 
+        newBoard[space] = board[pieceToMove]; // Move piece to new space.
+        newBoard[pieceToMove] = null; // Set old space to empty.
 
-        setPieceToMove(null);
+        setPieceToMove(null); // Clear selected piece.
         setBoard(newBoard);
       }, 200);
   }
 
+  // Converts a given move to a string in the format "BP to a5".
   function moveToString(space) {
     let letters = 'abcdefgh';
     return(board[pieceToMove] + " to " + letters[space%8] + (Math.floor(space/8) + 1));
@@ -76,13 +78,15 @@ const Board = () => {
     else setColorToMove("B"); 
   }
 
+  // Creates a square on the board given a background color, piece image, and animation. 
   function makeSquare(space, color, image, animatePiece) {
     let animation = "";
     
     try {
-  
+      // Setup movement animation
       let xDifference = (selectedSpace % 8) - (pieceToMove % 8)
       let yDifference = Math.floor(selectedSpace/8) - Math.floor(pieceToMove/8);
+      // Apply if the piece is animating.
       if (animatePiece && pieceToMove == space) {
         animation = `translate3d(${xDifference * 75}px, ${
           yDifference * 75
--- src/components/lib/movement.js
+++ src/components/lib/movement.js
@@ -1,3 +1,7 @@
+/*
+    This file contains all code used for calculating piece moves.
+*/
+
 const movement = {
 
     containsPiece(spaceIndex, board) {
@@ -40,6 +44,8 @@ const movement = {
 
     },
 
+    
+    //Returns a dictionary that contains the number of spaces to the edge of the board in every direction for each space.
     calculatePositionData() {
         let spacesToEdge = [];
         
@@ -73,6 +79,7 @@ const movement = {
         return spacesToEdge;
     },
 
+    // Gets all possible moves for a sliding piece (Rooks, Bishops, Queens).
     getSlidingMoves(pieceIndex) {
         let spacesToEdge = this.calculatePositionData();
         
@@ -107,6 +114,11 @@ const movement = {
         }
     },
 
+    /*
+     A utility function for checking the valid moves of each of the sliding pieces. 
+     Given an array of moves in a certain direction, it cuts of the array if it encounters another piece so that
+     the sliding piece cannot move past it.
+     */ 
     checkDirectionsForValidMoves(pieceIndex, directionsArray, board) {
         let availableMoves = []
         for (let directionIndex = 0; directionIndex < directionsArray.length; directionIndex++) {
@@ -114,11 +126,11 @@ const movement = {
                 
                 let spaceIndex = directionsArray[directionIndex][i]
                 
-                if (this.containsFriendlyPiece(pieceIndex, spaceIndex, board)) break;
+                if (this.containsFriendlyPiece(pieceIndex, spaceIndex, board)) break; // Cut off the array immediately if it is blocked by a friendly piece.
                 
                 availableMoves.push(spaceIndex)
 
-                if (this.containsEnemyPiece(pieceIndex, spaceIndex, board)) break;
+                if (this.containsEnemyPiece(pieceIndex, spaceIndex, board)) break; // Add the space fist before cutting it off if it contains an enemy piece so that it may be captured.
             }          
         }
         return availableMoves;
@@ -126,6 +138,7 @@ const movement = {
 
     getQueenMoves(pieceIndex, board) {
         let slidingMoves = this.getSlidingMoves(pieceIndex);
+        // Queens can move in both cardinal and ordinal directions.
         let possibleMoves = [slidingMoves.north, slidingMoves.south, slidingMoves.west, slidingMoves.east,
                             slidingMoves.northWest, slidingMoves.southWest, slidingMoves.northEast, slidingMoves.southEast]
 
@@ -134,6 +147,7 @@ const movement = {
 
     getRookMoves(pieceIndex, board) {
         let slidingMoves = this.getSlidingMoves(pieceIndex);
+        // Rooks can move only in cardinal directions.
         let possibleMoves = [slidingMoves.north, slidingMoves.south, slidingMoves.west, slidingMoves.east]
 
         return this.checkDirectionsForValidMoves(pieceIndex, possibleMoves, board);
@@ -141,16 +155,19 @@ const movement = {
 
     getBishopMoves(pieceIndex, board) {
         let slidingMoves = this.getSlidingMoves(pieceIndex);
+        // Bishops can move only in ordinal directions.
         let possibleMoves = [slidingMoves.northWest, slidingMoves.southWest, slidingMoves.northEast, slidingMoves.southEast]
 
         return this.checkDirectionsForValidMoves(pieceIndex, possibleMoves, board);
     },
 
     getKingMoves(pieceIndex, board) {
+        // Kings can move in any direction, but can only move one space at a time.
         let slidingMoves = this.getSlidingMoves(pieceIndex)
         let directions = Object.keys(slidingMoves)
         let availableMoves = [];
         for (let i = 0; i < directions.length; i++) {
+            // For each direction, push the first space to availableMoves if it does not contain a friendly piece.
             let spaceIndex = slidingMoves[directions[i]][0]
             if (!this.containsFriendlyPiece(pieceIndex, spaceIndex, board)) availableMoves.push(spaceIndex) 
         }
@@ -160,22 +177,29 @@ const movement = {
     getKnightMoves(pieceIndex, board) {
         let slidingMoves = this.getSlidingMoves(pieceIndex);
         
+        /*
+        The knight moves in an "L" shape, two spaces forward in any cardinal direction, and then one spaces to the side. 
+        */
+
+        // Start by getting spaces diagonally adjacent to the piece.
         let ordinalAdjacentSpaces = [slidingMoves.northWest[0], slidingMoves.southWest[0], slidingMoves.northEast[0], slidingMoves.southEast[0]]
+        // For each diagonal space, we need to check the space above or below it, and the space either to the right or left of it.
+        // These arrays tell us which spaces need to be checked for each index space index.
         let directionsX = [+1, +1, -1, -1]
         let directionsY = [-1, +1, -1, +1]
         
         let availableMoves = []
 
         for (let directionIndex = 0; directionIndex < 4; directionIndex++) {
-            let xSpaceIndex = ordinalAdjacentSpaces[directionIndex] + directionsX[directionIndex]
+            let xSpaceIndex = ordinalAdjacentSpaces[directionIndex] + directionsX[directionIndex] // Check space right or left of diagonal space.
             if (((directionsX[directionIndex] < 0 && slidingMoves.east.length >= 2) ||
-                (directionsX[directionIndex] > 0 && slidingMoves.west.length >= 2)) &&
+                (directionsX[directionIndex] > 0 && slidingMoves.west.length >= 2)) && // Make sure space is on the board.
                 !this.containsFriendlyPiece(pieceIndex, xSpaceIndex, board))
             availableMoves.push(xSpaceIndex)
             
-            let ySpaceIndex = ordinalAdjacentSpaces[directionIndex] + (directionsY[directionIndex] * 8)
+            let ySpaceIndex = ordinalAdjacentSpaces[directionIndex] + (directionsY[directionIndex] * 8) // Check space above or below diagonal space.
             if (((directionsX[directionIndex] < 0 && slidingMoves.east.length >= 1) ||
-                (directionsX[directionIndex] > 0 && slidingMoves.west.length >= 1)) &&
+                (directionsX[directionIndex] > 0 && slidingMoves.west.length >= 1)) && // Make sure space is on the board.
                 !this.containsFriendlyPiece(pieceIndex, ySpaceIndex, board))
             availableMoves.push(ySpaceIndex)
         }
@@ -187,7 +211,13 @@ const movement = {
         let slidingMoves = this.getSlidingMoves(pieceIndex);
         let availableMoves = []
 
-        let yDirection;
+        /*
+        Pawns can move one space forward, but only if that space is completely unoccupied.
+        They can also move two spaces forward in the same way if they are being moved for the first time.
+        However, pawns can only capture pieces in front of them diagonally.
+        */
+
+        let yDirection; // Get the direction that the pawn can move in based on its color.
         if (this.pieceIsWhite(pieceIndex, board)) yDirection = "south";
         else yDirection = "north";
 
@@ -195,10 +225,12 @@ const movement = {
         if (!this.containsPiece(spaceIndex, board)) {
             availableMoves.push(spaceIndex)
             spaceIndex = slidingMoves[yDirection][1]
+            // Pawn can move 2 spaces if it has not been moved and that space is free.
             if (!this.containsPiece(spaceIndex, board) && !this.pawnHasMoved(pieceIndex, board))
             availableMoves.push(spaceIndex)
         }
 
+        // Check if forward diagonal pieces contain enemies. 
         spaceIndex = slidingMoves[yDirection + "East"][0]
         if (this.containsEnemyPiece(pieceIndex, spaceIndex, board)) availableMoves.push(spaceIndex)
         spaceIndex = slidingMoves[yDirection + "West"][0 ]
